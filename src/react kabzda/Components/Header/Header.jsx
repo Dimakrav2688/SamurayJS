e4dc41396f58cfd9f4f0225b427c0681d8e7fc6d
Dimakrav2688 <dimakrav2688@gmail.com>
2021-12-11 15:33:58 +0200

    lesson 66-1

--- src/react kabzda/Components/Header/Header.jsx
+++ src/react kabzda/Components/Header/Header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
 
-const Header = (props) => {
+const Header = ({isAuth, login}) => {
   return (
     <header className={s.header}>
       <img src='https://st2.depositphotos.com/1192512/6870/v/600/depositphotos_68706365-stock-illustration-woman-eye-logo-beauty-symbol.jpg' />
       <div className={s.loginBlok}>
-        {props.isAuth ? props.login
+        {isAuth ? login
         : <NavLink to={'/login'}>Login</NavLink> }
       </div>
     </header>
