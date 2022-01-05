89d16fd8b1e05ee7b4e3ba1804b36e2de81a0635
Dimakrav2688 <dimakrav2688@gmail.com>
2022-01-05 21:14:07 +0200

    lesson 94 Lazy\Suspanse

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
-import DialogsContainer from './react kabzda/Components/Dialogs/DialogsContainer';
 import NavBar from './react kabzda/Components/Navbar/NavBar';
-import ProfileContainer from './react kabzda/Components/Profile/ProfileContainer';
 import UsersContainer from './react kabzda/Components/Users/UsersContainer';
 import Music from './react kabzda/Components/Navbar/Music';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HeaderContainer from './react kabzda/Components/Header/HeaderContainer';
 import Login from './react kabzda/Components/Login/Login';
 import { connect } from 'react-redux';
@@ -13,10 +11,12 @@ import { initializeApp } from './react kabzda/redux/app-reduser'
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
 import Preloader from './react kabzda/Components/Common/Preloader/Preloader';
+let DialogsContainer = lazy(() => import('./react kabzda/Components/Dialogs/DialogsContainer'));
+let ProfileContainer = lazy(() => import('./react kabzda/Components/Profile/ProfileContainer'));
 
 class App extends Component {
   componentDidMount() {
-    this.props.initializeApp()      
+    this.props.initializeApp()
   }
 
   render() {
@@ -25,30 +25,33 @@ class App extends Component {
     }
 
     return (
-      
-        <div className='app-wrapper'>
-          <HeaderContainer />
-          <NavBar />
-          <div className='app-wrapper-content'>
 
-            <Route path='/dialogs'
-              render={() => <DialogsContainer />} />
-
-            <Route path='/profile/:userId?'
-              render={() => <ProfileContainer />} />
-
-            <Route path='/users'
-              render={() => <UsersContainer />} />
-
-            <Route path='/login'
-              render={() => <Login />} />
+      <div className='app-wrapper'>
+        <HeaderContainer />
+        <NavBar />
+        <div className='app-wrapper-content'>
+          <Suspense fallback={<Preloader />}>
+            <Switch >
+              <Route path='/dialogs' component={DialogsContainer} />
+              <Route path='/profile/:userId?' component={ProfileContainer} />
+            </Switch>
+          </Suspense>
 
 
-            <Route path='/news' component={NavBar} />
-            <Route path='/music'> <Music /> </Route>
-            <Route path='/setings' component={NavBar} />
-          </div>
-        </div>      
+
+
+          <Route path='/users'
+            render={() => <UsersContainer />} />
+
+          <Route path='/login'
+            render={() => <Login />} />
+
+
+          <Route path='/news' component={NavBar} />
+          <Route path='/music'> <Music /> </Route>
+          <Route path='/setings' component={NavBar} />
+        </div>
+      </div >
     );
   }
 }
@@ -62,4 +65,3 @@ export default compose(
   connect(mapStateToProps, { initializeApp }))(App);
 
 
-  
